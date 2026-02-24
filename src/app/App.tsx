4353335dbcf62c79eafecab6542b8c52034c3a05
yujinimda <s0912135@gmail.com>
2026-02-24 23:59:09 +0900

    feat: hero 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import Skills from "../widgets/Skills/Skills";
 import Hero from "../widgets/Hero/Hero";
+import Hero6 from "../widgets/Hero/Hero6";
 
 function App() {
   return (
     <>
       <Hero />;
+      <Hero6 />;
       <Skills />;
     </>
   );
